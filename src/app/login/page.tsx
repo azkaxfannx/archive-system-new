46a88c1cb5da57238552566a015cae6301abfae1
azkaxfannx <azkamaulana6789@gmail.com>
2025-09-09 09:05:44 +0700

    minor update nip

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -79,7 +79,7 @@ export default function LoginPage() {
         <form onSubmit={onSubmit} className="space-y-4">
           <input
             className="border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Nip"
+            placeholder="NIP"
             type="text"
             value={nip}
             onChange={(e) => setNip(e.target.value)}
--- src/app/njir/page.tsx
+++ src/app/njir/page.tsx
@@ -44,7 +44,7 @@ export default function RegisterPage() {
           />
           <input
             className="border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Nip"
+            placeholder="NIP"
             type="text"
             value={nip}
             onChange={(e) => setNip(e.target.value)}
