425f447b52108d028c357e6071ac86675fd93c64
azkaxfannx <azkamaulana6789@gmail.com>
2025-09-16 19:36:37 +0700

    lagi

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "archive-system-new",
-      script: "pnpm",
-      args: "start",
+      script: "node_modules/.bin/next",
+      args: "start -H 0.0.0.0 -p 3000",
       cwd: __dirname,
       instances: 1,
       autorestart: true,
